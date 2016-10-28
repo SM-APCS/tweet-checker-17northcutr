4b9023dcdd0111e4bcf6a5503f894d5b5b11d85c
T.Rex Northcut <17northcutr@smtexas.org>
2016-10-28 13:54:55 -0500

    I made the code

--- TweetTester/src/tweettester/TweetTester.java
+++ TweetTester/src/tweettester/TweetTester.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tweettester;
 
 import java.util.Scanner;
 import java.lang.Math; 
 /**
  * @class  AP CSA
- * @author INSERT YOUR NAME HERE
+ * @author T.Rex Northcut
  */
 public class TweetTester {
 
@@ -17,10 +12,14 @@ public class TweetTester {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner scan = new Scanner (System.in);
-         String tweet;
-         
+        Scanner scan = new Scanner(System.in); //read the input
+        String tweet = scan.nextLine(); //save the input into a string
+        int count=0;
+        //increase the count variable for every character in the string
+        for(int i=0; i<tweet.length(); i++){
+            count++;
+        }
+        //return the number of characters
+        System.out.println("Number of Characters: " + count);
     }
-    
 }
